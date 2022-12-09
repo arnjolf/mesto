f27600a974cbee10ef70025277d941881c7cc3b0
arnjolf <arnjolff@gmail.com>
2022-12-09 22:57:28 +0600

    refactor: popup; style: margins header;

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close');
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('#popup__name');
+let jobInput = formElement.querySelector('#popup__job');
+
+let popup = document.querySelector('.popup');
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;;
+  jobInput.value = profileJob.textContent;;
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;;
+  profileJob.textContent = jobInput.value;;
+  popupClose();
+}
+
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+formElement.addEventListener('submit', handleFormSubmit);
